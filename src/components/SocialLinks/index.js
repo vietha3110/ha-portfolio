56a12f207e291278ba3dd3ec7907f7ceb74cc04d
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-04-26 16:22:13 -0700

    Updated resume link.

--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                    Resume <BsFillPersonLinesFill size={30}/>
                 </>
             ),
-            href: 'https://github.com/vietha3110',
+            href: 'https://drive.google.com/file/d/1_JoS21xBFpKfuVZIzpO2EP3QgysKWfGY/view?usp=sharing',
             style: 'rounded-tr-md'
         }
     ]
